389eb5dda1384fb5000c2828da8999de042a4203
jadebc <jadebc@stanford.edu>
2024-10-18 17:09:30 -0700

    add surface water

--- 2-figure-scripts/plot-water-level-flood.R
+++ 2-figure-scripts/plot-water-level-flood.R
@@ -6,21 +6,20 @@
 rm(list=ls())
 source(paste0(here::here(), '/0-config.R'))
 
+d = readRDS(paste0(data_dir, "/baseline_clean.RDS"))
+
 gov_data_raw = read.csv("/Users/jadebc/Library/CloudStorage/Box-Box/Jade Benjamin-Chung's Externally Shareable Files/CRADLE-Data/Climate Data Sirajganj/climate_data_sirajganj.csv")
 
 gov_data = filter(gov_data_raw, !is.na(Sirajganj_WL)) %>% 
-  mutate(date = as.Date(date, format = "%Y-%m-%d"))
-
-# make a plot of the water level over time 
-ggplot(gov_data, aes(x=date, y=Sirajganj_WL)) + geom_line() + 
-  xlab("Date") + ylab("Water Level (m)")
+  mutate(date = as.Date(date, format = "%Y-%m-%d")) %>% 
+  filter(date >= as.Date("2023-04-01"))
 
 baseline <- readRDS(paste0(data_dir, "baseline_clean.RDS")) %>% 
   dplyr::select(dataid, flood_compound, date_compound_flood, 
                 flood_union, date_union_flood,
                 latrine_flooded)
 
-flood_data <-   baseline %>% filter(flood_compound == 1 | flood_union == 1) %>% 
+flood_data <-   d %>% filter(flood_compound == 1 | flood_union == 1) %>% 
   group_by(date_union_flood) %>%
   summarise(n_union_flood = sum(as.numeric(as.character(flood_union), na.rm=T)),
             n_compound_flood = sum(as.numeric(as.character(flood_compound), na.rm=T)),
@@ -36,12 +35,12 @@ WL_flood <- left_join(gov_data, flood_data,
 
 # make a plot of the water level over time 
 plot_data = WL_flood %>% filter(date >as.Date('2023-01-01') &
-                                  date <as.Date('2024-01-01'))
+                                  date <as.Date('2024-03-01'))
 
 
 water_plot <- ggplot(plot_data, aes(x=date, y = Sirajganj_WL)) + 
   geom_line(data=WL_flood %>% filter(date >as.Date('2023-01-01') &
-                                       date <as.Date('2024-01-01')), aes(x=date, y=Sirajganj_WL)) + 
+                                       date <as.Date('2024-03-01')), aes(x=date, y=Sirajganj_WL)) + 
   geom_area(fill = "#498fd1") +
   geom_point(data = plot_data %>% filter(any_flood==1 ),
              aes(x = date, y = Sirajganj_WL),size=1) +
@@ -53,7 +52,8 @@ water_plot <- ggplot(plot_data, aes(x=date, y = Sirajganj_WL)) +
         panel.grid.minor.x = element_blank(),
         panel.grid.major.x = element_blank(),
         panel.grid.major.y = element_line(size=0.2),
-        panel.grid.minor.y = element_line(size=0.1))
+        panel.grid.minor.y = element_line(size=0.1)) +
+  ggtitle("B)")
 
 
 # make a plot of the rainfall over time 
@@ -68,11 +68,43 @@ rain_plot <- ggplot(plot_data, aes(x=date, y = Rainfall)) +
         panel.grid.major.x = element_blank(),
         panel.grid.major.y = element_line(size=0.2),
         panel.grid.minor.y = element_line(size=0.1),
-        axis.title.x = element_blank())
+        axis.title.x = element_blank()) +
+  ggtitle("A)")
 
-# combine and save plots
-combined_plot <- grid.arrange(rain_plot, water_plot, ncol=1)
+# make a plot of % of seasonal water by month 
+colors <- c(
+
+  "#E0A17E", # April
+  "#E8916C", # May
+  "#C1A7A7", # June
+  "#45B7D1", # July
+  "#5AAFC5", # August
+  "#6FA7B9", # September
+  "#849FAD", # October
+  "#9997A1", # November
+  "#AEAEB6",  # December
+  "#E0E0E0", # January
+  "#D4D7E5", # February
+  "#F0D6A3" # March
+)
+
+d = d %>% mutate(month_f = factor(month, levels = c(
+  4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 2, 3
+)))
+
+surface_water_plot <- ggplot(d, aes(x=month_f, y = prop_seasonal_water_1000))+
+  geom_violin(aes(fill = month_f)) +
+  scale_fill_manual(values = colors) + 
+  xlab("Month") + 
+  ylab("Proportion of area within 1km\nof compound with seasonal water") +
+  theme_minimal() +
+  theme(legend.position = "none",
+        panel.grid.major.x = element_blank()) +
+  ggtitle("C)")
+
+combined_plot <- grid.arrange(rain_plot, water_plot,surface_water_plot, ncol=1)
+
 
 ggsave(combined_plot, filename = paste0(figure_path, "plot_rainfall_water_level.pdf"),
-       width = 5, height = 5, units = "in")
+       width = 5, height = 7, units = "in")
 
